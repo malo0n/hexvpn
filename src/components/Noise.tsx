6075c7f0e17bced17c3a2f4a899bd1765d6f4f40
Greg <k0ll0bahob@gmail.com>
2025-02-16 13:27:58 +0300

    hexFigure fix

--- src/components/Noise.tsx
+++ src/components/Noise.tsx
@@ -5,8 +5,8 @@ export const Noise = () => {
     <>
       <div className='fixed inset-0 -z-10 h-[100vh]  flex-none bg-blackMain'>
         <div
-          style={{ backgroundImage: `url(/noise.gif)`, backgroundSize: "180px" }}
-          className='h-full min-h-full bg-repeat opacity-[0.11]'
+          style={{ backgroundImage: `url(/noise.gif)`, backgroundSize: "200px" }}
+          className='h-full min-h-full bg-repeat opacity-[0.09]'
         ></div>
       </div>
     </>
